f6bb663e1e661b0a24a55157445fa93ca8f3f1b3
AnikSen <37754808+aniksen0@users.noreply.github.com>
2025-02-14 22:15:02 +0600

    first commit

---
+++ tests/example.spec.js
@@ -0,0 +1,18 @@
+// @ts-check
+import { test, expect } from '@playwright/test';
+test.beforeEach(async ({ page }) => {
+  // Open the page before each test.
+  await page.goto('/');
+});
+test('Landed on evaly home page', async ({ page }) => {
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle("Evaly | Online shopping platform");
+});
+
+test('Login has all the input fields', async ({ page }) => {
+
+  // Click the get login link.
+  let login_element = await page.waitForSelector("//span[@class='sr-only'][normalize-space()='Sign in']");
+  await login_element.click();
+  await page.pause(5000);
+});
